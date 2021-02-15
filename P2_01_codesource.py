da8fbf72109430104be5c8a13b95d74056495b56
Djodre88 <jodre.diangienda@gmail.com>
2021-02-15 23:22:46 +0100

    Premiers parsings via bs4

--- P2_01_codesource.py
+++ P2_01_codesource.py
@@ -7,7 +7,20 @@ header=['product_page_url','universal_product_code(upc)','title','price_includin
 l=len(header)
 datas=['']*l
 
-site_url="https://books.toscrape.com/"                    
+site_url="https://books.toscrape.com/"               
                     
-                   
 book_url='https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+
+req = requests.get(book_url)
+soup = BeautifulSoup(req.content, "html.parser")
+
+#Product_page_url
+datas[0]=book_url
+
+#universal_product_code (upc)
+upc=soup.tr.td
+datas[1]=upc.text
+
+#Title
+title=soup.h1
+datas[2]=title.text
